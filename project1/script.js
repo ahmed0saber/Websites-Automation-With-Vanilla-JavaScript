0a38fb4897fdd597ae137100e51acacf06d313f6
Ahmed Saber <ahmed0saber33@gmail.com>
2022-01-05 15:15:18 +0200

    Add files via upload

---
+++ project1/script.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    document.getElementById("myFrame").contentWindow.document.getElementsByTagName("input")[1].checked = true;
+    document.getElementById("myFrame").contentWindow.document.getElementsByTagName("input")[2].value = "ahmed0saber";
+    document.getElementById("myFrame").contentWindow.document.getElementsByTagName("input")[3].value = "0123456789";
+}
+function myFunction2(){
+    document.getElementById("myFrame").contentWindow.document.getElementsByTagName("input")[4].click();
+}
